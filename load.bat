e031063771d686e6ce82e583787a2b151dc65cd1
Kaio HSG <kaio12hsg@gmail.com>
2025-05-15 12:45:26 -0300

    Add files via upload

--- load.bat
+++ load.bat
@@ -1,7 +1,7 @@
 @echo off
-title TCGRW Backup Load
 set "id=c553bb55-9c7c-4647-b7e6-09c1fbe31ee8"
 set "savePath=C:\Program Files (x86)\Ubisoft\Ubisoft Game Launcher\savegames"
+title TCGRW Backup Load
 pushd %savePath%\%id%
 echo Loading backup...
 tar -xf "%userProfile%\TCGRW Save.tar"
--- start.bat
+++ start.bat
@@ -1,11 +1,11 @@
 @echo off
 set "id=c553bb55-9c7c-4647-b7e6-09c1fbe31ee8"
 set "savePath=C:\Program Files (x86)\Ubisoft\Ubisoft Game Launcher\savegames"
-title TCGRW Backup Save
+title TCGRW Backup Start
 pushd "%savePath%\%id%"
 echo Saving backup...
 tar -cf "%userProfile%\TCGRW Save.tar" "3559"
-echo ##################################################
-echo Finish.
-pause > nul
+echo --------------------------------------------------
+echo Starting game...
+start "" "%~dp0\.exe"
 exit
